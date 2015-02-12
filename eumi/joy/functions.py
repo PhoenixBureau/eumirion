653fec50f97f5d3147bac542ca14eaecab94c0f5
Simon Forman <sforman@hushmail.com>
2015-02-12 11:58:30 -0800

    Change joy.py to Eumirion.

--- eumi/joy/functions.py
+++ eumi/joy/functions.py
@@ -2,20 +2,20 @@
 #
 #    Copyright © 2014, 2015 Simon Forman
 #
-#    This file is part of joy.py
+#    This file is part of Eumirion
 #
-#    joy.py is free software: you can redistribute it and/or modify
+#    Eumirion is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
 #    the Free Software Foundation, either version 3 of the License, or
 #    (at your option) any later version.
 #
-#    joy.py is distributed in the hope that it will be useful,
+#    Eumirion is distributed in the hope that it will be useful,
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of
 #    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #    GNU General Public License for more details.
 #
 #    You should have received a copy of the GNU General Public License
-#    along with joy.py.  If not see <http://www.gnu.org/licenses/>.
+#    along with Eumirion.  If not see <http://www.gnu.org/licenses/>.
 #
 '''
 

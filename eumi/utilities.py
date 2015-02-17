6d21bb737d8db697975ff201236fe07f22a65ee8
Simon Forman <sforman@hushmail.com>
2015-02-17 07:19:49 -0800

    Experimenting...

---
+++ eumi/utilities.py
@@ -0,0 +1,42 @@
+from collections import defaultdict
+from pprint import pformat
+from eumi.main import pather, Page, read_pickle
+from eumi.joy.stack import strstack
+from eumi.page_actions import scan_text, linkerate, l
+
+
+pickle_name = 'server.pyckle'
+server = read_pickle(pickle_name)
+server.debug = True
+
+
+def print_server(server):
+  data = {}
+  for (kind, unit), page_data in server.router.iteritems():
+    key = linkerate(kind, unit)
+    page_data = page_data.data
+    page = data[key] = {
+      'text': page_data['text'],
+      'title': page_data['title'],
+      }
+    if 'joy' in page_data:
+      page['joy'] = strstack(page_data['joy'])
+  return data
+
+
+def build_dependency_graph(data):
+  deps = {}
+  for key, page_data in data.iteritems():
+    page_deps = defaultdict(set)
+    text = page_data['text']
+    for action, kind, unit in scan_text(text):
+      if action == 'text' and unit is None:
+        continue
+      page_deps[action].add(l(kind, unit))
+    deps[key] = dict(page_deps)
+  return deps
+
+
+if __name__ == '__main__':
+  print pformat(print_server(server))
+#  print pformat(build_dependency_graph(print_server(server)))
